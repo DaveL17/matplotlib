8a50a97dea2d4bd88830ef65485d1bb5ba13f197
DaveL17 <dave.lehrer@icloud.com>
2017-11-20 19:46:31 -0600

    Commits scripting example

---
+++ scripting.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+matplotlibPlugin = indigo.server.getPlugin("com.fogbert.indigoplugin.matplotlib")
+payload = {'x_values': [1, 2, 3],
+           'y_values': [2, 4, 7],
+           'kwargs': {'linestyle': 'dashed',
+                      'color': 'b',
+                      'marker': 'd',
+                      'markerfacecolor': 'r'},
+           'path':'/Library/Application Support/Perceptive Automation/Indigo 7/IndigoWebServer/images/controls/static/',
+           'filename': 'chart_filename.png'
+           }
+try:
+    result - matplotlibPlugin.executeAction('refreshTheChartsAPI', deviceId=0, waitUntilDone=True, props=payload)
+    if result is not None:
+        indigo.server.log(result['message'])
+except Exception as err:
+    indigo.server.log(u"Exception occurred: {0}".format(err))
